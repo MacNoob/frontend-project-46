02882cf391610a2fb7b3cc21f462464a89b00788
MacNoob <mcmagus@mail.ru>
2022-12-12 19:14:43 +0300

    Fix linter rulles

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url';
 import * as fs from 'node:fs';
 import path, { dirname } from 'path';
+// eslint-disable-next-line import/extensions
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,8 +10,10 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+// eslint-disable-next-line no-undef
 test('genDiff', () => {
   const expectedData = readFile('expected.txt');
   const actualData = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  // eslint-disable-next-line no-undef
   expect(actualData).toEqual(expectedData);
 });
